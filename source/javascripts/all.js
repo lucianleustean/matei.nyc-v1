9f1dbb92b7abe25447083ee95b8e3b4fe19db96c
lucianleustean <lucian@kabisa.nl>
2016-08-02 20:45:23 +0200

    Add scrolling effect for page navigation

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,19 @@
-// This is where it all goes :)
+function animate(elem, style, unit, from, to, time, prop) {
+    if (!elem) return;
+    var start = new Date().getTime(),
+        timer = setInterval(function() {
+            var step = Math.min(1, (new Date().getTime()-start)/time);
+            if (prop) {
+                elem[style] = (from+step*(to-from))+unit;
+            } else {
+                elem.style[style] = (from+step*(to-from))+unit;
+            }
+            if( step == 1) clearInterval(timer);
+        }, 1);
+    elem.style[style] = from+unit;
+}
+
+window.onload = function () {
+    var target = document.getElementById("container");
+    animate(document.body, "scrollTop", "", 0, target.offsetTop, 500, true);
+};
