d6fde86d8fd6a3f5e9912cd7aa5bd15aef073222
lucianleustean <lucian@kabisa.nl>
2016-08-03 20:47:56 +0200

    addEventListener for nav links url

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,4 @@
+// Scroll effect on navigation
 function animate(elem, style, unit, from, to, time, prop) {
     if (!elem) return;
     var start = new Date().getTime(),
@@ -17,3 +18,23 @@ window.onload = function () {
     var target = document.getElementById("container");
     animate(document.body, "scrollTop", "", 0, target.offsetTop, 500, true);
 };
+
+// background-image on mouse over navigation
+var navUrls = document.getElementsByClassName("nav");
+
+for (var i = 0; i < navUrls.length; i++) {
+    navUrls[i].addEventListener("mouseover", mouseOver, false);
+    navUrls[i].addEventListener("mouseout", mouseOut, false);
+}
+
+function mouseOver (e) {
+    var className = e.target.attributes.data.value;
+    document.getElementById("header").classList.add("hovered");
+    document.getElementById("header").setAttribute("style", "background-image: url('/images/intro/" + className + ".jpg');");
+}
+
+function mouseOut (e) {
+  var className = e.target.attributes.data.value;
+  document.getElementById("header").classList.remove("hovered");
+  document.getElementById("header").removeAttribute("style", "background-image: url('/images/intro/" + className + ".jpg');");
+}
